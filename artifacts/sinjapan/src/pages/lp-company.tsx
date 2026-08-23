85282a0e2aefe9745e47fdab79aad6fab77c2de7
SIN JAPAN LLC <japan@sinllc.jp>
2026-08-23 10:29:33 +0000

    LP/LP-company: store badges top-right on mobile (absolute)

--- artifacts/sinjapan/src/pages/lp-company.tsx
+++ artifacts/sinjapan/src/pages/lp-company.tsx
@@ -53,7 +53,7 @@ export default function LPCompany() {
         </div>
 
         {/* Store Badges */}
-        <div className="absolute bottom-14 right-3 md:right-20 flex flex-col items-end gap-2 z-20">
+        <div className="absolute top-20 right-3 md:top-auto md:bottom-14 md:right-20 flex flex-col items-end gap-2 z-20">
           <div className="relative select-none">
             <div className="flex items-center gap-2.5 bg-black rounded-xl px-4 py-2.5 w-[152px] opacity-35">
               <svg width="20" height="24" viewBox="0 0 24 24" fill="white"><path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.8-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/></svg>
--- artifacts/sinjapan/src/pages/lp.tsx
+++ artifacts/sinjapan/src/pages/lp.tsx
@@ -58,7 +58,7 @@ export default function LP() {
         </div>
 
         {/* Store Badges */}
-        <div className="absolute bottom-14 right-3 md:right-20 flex flex-col items-end gap-2 z-20">
+        <div className="absolute top-20 right-3 md:top-auto md:bottom-14 md:right-20 flex flex-col items-end gap-2 z-20">
           {/* App Store - 準備中 */}
           <div className="relative select-none">
             <div className="flex items-center gap-2.5 bg-black rounded-xl px-4 py-2.5 w-[152px] opacity-35">
